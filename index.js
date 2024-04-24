2da535fefec2f1d784adf3a96b3971c334e6cf36
Yago <almeidayago167@gmail.com>
2024-04-24 14:58:14 -0400

    complete array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(function(name) {
+        return name.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(nameLetter) {
+        return nameLetter.slice(0,2).toLowerCase().includes(string.toLowerCase())
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function(obj) {
+        return obj.name === string
+    })
+}
+
